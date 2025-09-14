b80ff5bbaf48e1a11486387c6dbdff6087f3d4ce
Sothyra12 <schan256@my.centennialcollege.ca>
2025-09-13 23:59:13 -0400

    rendered pug a static template

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 
 app.route('/').get((req, res) => {
-
+  res.render(index);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port http://localhost:${PORT}`);
 });
