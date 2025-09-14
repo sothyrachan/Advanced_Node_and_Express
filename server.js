c43adf83224a9538097b8d8758147fb6a97ad7de
Sothyra12 <schan256@my.centennialcollege.ca>
2025-09-14 01:03:53 -0400

    fixed setting the pug template

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
-
 
 app.route('/').get((req, res) => {
-  res.render(index);
+  // Change the response to render the Pug template
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
