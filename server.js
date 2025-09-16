1dcf8c5d19ddca0b280fbaac0328ee82cc743ab7
Sothyra12 <schan256@my.centennialcollege.ca>
2025-09-15 23:44:01 -0400

    edited server.js file

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.route('/').get((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port http://localhost:${PORT}`);
 });
