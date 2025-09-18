e67feb1e9b3618165f51a61807ee8597611a1d93
Sothyra Chan <macbook@Sothyras-macbookpro-2.local>
2025-09-17 23:55:05 -0400

    setup passport

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
 
 app.use(cors());
 app.set('view engine', 'pug');
@@ -13,11 +15,19 @@ fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}))
 
 app.route('/').get((req, res) => {
   // Change the response to render the Pug template
   res.render('index', {title: 'hello', message: 'Please log in'});
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
