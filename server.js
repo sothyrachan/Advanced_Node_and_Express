8397ea92b55d7b1f9709e6faa5d569b2ab7f50d7
sothyrachan <schan256@my.centennialcollege.ca>
2025-09-28 00:02:53 -0400

    tell passport to use the LocalStrategy

--- server.js
+++ server.js
@@ -47,6 +47,16 @@ myDB(async client => {
     });
   });
 
+  passport.use(new LocalStrategy((username, password, done) => {
+  myDataBase.findOne({ username: username }, (err, user) => {
+    console.log(`User ${username} attempted to log in.`);
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    if (password !== user.password) return done(null, false);
+    return done(null, user);
+  });
+}));
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
