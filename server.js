a1ab0efccbb3b8c11cf8edadcc0ee90d053112d4
sothyrachan <schan256@my.centennialcollege.ca>
2025-10-04 00:52:40 -0400

    fixed create new middleware step

--- server.js
+++ server.js
@@ -62,15 +62,11 @@ myDB(async client => {
     res.redirect('/profile');
   });
 
-  app.route('/profile').get((req, res) => {
-    res.render('/profile');
-  });
-
-app
- .route('/profile')
- .get(ensureAuthenticated, (req,res) => {
-    res.render('/views/pug/profile');
- });
+  app
+   .route('/profile')
+   .get(ensureAuthenticated, (req,res) => {
+      res.render('profile');
+   });
 
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
