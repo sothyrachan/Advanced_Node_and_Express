f3470146573f4106e69dc7607fc3df2d42a82fe5
sothyrachan <schan256@my.centennialcollege.ca>
2025-11-05 21:59:59 -0500

    step for communicating by emitting

--- server.js
+++ server.js
@@ -65,8 +65,11 @@ myDB(async client => {
   });
 });
 
+let currentUsers = 0;
 io.on('connection', socket => {
   console.log('A user has connected');
+  ++currentUsers;
+  io.emit('user count', currentUsers);
 });
 
 const PORT = process.env.PORT || 3000;
