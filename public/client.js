fb7bf86ebbb40e01515e534b020708b075c4c175
sothyrachan <schan256@my.centennialcollege.ca>
2025-12-09 23:40:59 -0500

    Trying to fix emit

--- public/client.js
+++ public/client.js
@@ -1,17 +1,18 @@
 // client.js
 
-/*global io*/
 $(document).ready(function () {
+  /*global io*/
   let socket = io();
 
-  socket.on("user count", function (count) {
-    console.log("Current Users:", count);
+  socket.on('user count', function (data) {
+    console.log(data);
   });
-
+  
   // Form submittion with new message in field with id 'm'
-  $("form").submit(function () {
-    var messageToSend = $("#m").val();
-    $("#m").val("");
+  $('form').submit(function () {
+    var messageToSend = $('#m').val();
+
+    $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
 });
--- server.js
+++ server.js
@@ -3,49 +3,28 @@
 'use strict';
 
 require('dotenv').config();
-require('socket.io');
-
-const routes = require('./routes.js');
-const auths = require('./auth.js');
 
 const express = require('express');
 const helmet = require('helmet');
-const myDB = require('./connection');
-const path = require("path");
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const app = express();
 const cors = require('cors');
 const session = require('express-session');
+const path = require("path");
+const myDB = require('./connection');
+const routes = require('./routes.js');
+const auths = require('./auth.js');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
-// const passport = require('passport');
-// const { ObjectID } = require('mongodb');
-// const LocalStrategy = require('passport-local');
-// const bcrypt = require('bcrypt');
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch([
-  path.join(__dirname, "views"),   // Pug templates
-  path.join(__dirname, "public")   // Static files (CSS, JS)
-]);
-
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 50);
-});
-
-// 2. Inject the LiveReload script into the HTML
-app.use(connectLivereload());
 
 app.use(helmet());
 app.use(cors());
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); // For fCC testing purposes
+
+fccTesting(app); // FreeCodeCamp testing hook
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -53,25 +32,28 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: {secure: false}
+  cookie: { secure: false }
 }));
-
-let currentUsers = 0;
-
-io.on('connection', socket => {
-  currentUsers++;
-  io.emit('user count', currentUsers);
-
-  socket.on('disconnect', () => {
-    currentUsers--;
-    io.emit('user count', currentUsers);
-  });
-});
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   routes(app, myDataBase);
   auths(app, myDataBase);
+
+  // FCC test-required feature
+  let currentUsers = 0;
+
+  io.on('connection', (socket) => {
+    currentUsers++;
+    io.emit('user count', currentUsers);
+    console.log("User connected. Total:", currentUsers);
+
+    socket.on('disconnect', () => {
+      currentUsers--;
+      io.emit('user count', currentUsers);
+      console.log("User disconnected. Total:", currentUsers);
+    });
+  });
 
 }).catch(e => {
   app.route('/').get((req, res) => {
@@ -81,5 +63,5 @@ myDB(async client => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log(`Listening at http://localhost:${PORT}`);
 });
