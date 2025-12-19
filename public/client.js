f4c80450e464946aba013b249cc854f40d90a78d
Sothyra Chan <schan256@my.centennialcollege.ca>
2025-12-19 04:28:53 +0000

    send and display chat message

--- public/client.js
+++ public/client.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
-
+    socket.emit('chat message', messageToSend);
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
